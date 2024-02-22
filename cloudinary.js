0f242b3a2c315d56a1219fb9bb2e8f834d9dbc9e
Baibhav Kumar <cocc1274@gmail.com>
2024-02-22 15:22:03 +0530

    fix: update keys

--- cloudinary.js
+++ cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
-
+require('dotenv'.config())
 cloudinary.config({
-    cloud_name:'drppgvivt',
-    api_key:'953588446692354',
-    api_secret:'GIeVUSC4epJjSJtv0z53yDmQ1jU'
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET
 })
 
 module.exports= cloudinary
